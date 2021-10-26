25dd11ef88a3cadf64b8681aedb233d8065e7fe8
Pierre Glandon <pglandon78@gmail.com>
2021-10-26 11:54:31 +0200

    add typescript to app and shared

--- app/index.js
+++
@@ -1,5 +0,0 @@
-import { callback } from './shared/clickCallback.js';
-
-document
-  .getElementById("button-id")
-  .addEventListener("click", callback);
---
+++ app/index.ts
@@ -0,0 +1,3 @@
+import { callback } from "./shared/clickCallback";
+
+document.getElementById("button-id").addEventListener("click", callback);
