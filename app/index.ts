93c974ef1336354cacf8bf36797f34e2e7c1af96
Pierre Glandon <pglandon78@gmail.com>
2021-11-12 15:36:14 +0100

    feat: add unrecognized import

--- app/index.ts
+++ app/index.ts
@@ -1,3 +1,4 @@
+import { callback } from 'shared';
 const x: number = 5;
 
-document.getElementById("button-id").addEventListener("click", () => alert("Hello, World!"));
+document.getElementById("button-id").addEventListener("click", callback);
