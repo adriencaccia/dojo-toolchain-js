2c864d67bfef7f16f2aea55df4277a8bc1095837
Guillaume Vagner <guillaume.vagner@gmail.com>
2021-11-17 14:53:18 +0100

    --wip-- [skip ci]

--- shared/babel.config.js
+++ shared/babel.config.js
@@ -1,6 +1,39 @@
-module.exports = {
-    presets: [
-        '@babel/preset-typescript',
-        '@babel/preset-env'
-    ]
-}
+// shared/babel.config.js
+
+const defaultPresets = [
+    ["@babel/preset-typescript"],
+  ];
+  
+  const defaultIgnores = ["node_modules", "lib"];
+  
+  const presetsForESM = [
+    [
+      "@babel/preset-env",
+      {
+        modules: false,
+      },
+    ],
+    ...defaultPresets,
+  ];
+  const presetsForCJS = [
+    [
+      "@babel/preset-env",
+      {
+        modules: "cjs",
+      },
+    ],
+    ...defaultPresets,
+  ];
+  
+  module.exports = {
+    env: {
+      cjs: {
+        ignore: defaultIgnores,
+        presets: presetsForCJS,
+      },
+      esm: {
+        ignore: defaultIgnores,
+        presets: presetsForESM,
+      },
+    },
+  };
